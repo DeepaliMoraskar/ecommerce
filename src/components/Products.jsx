6604d4abb990460828a53aa2fe0db7ab2cd88454
DeepaliMoraskar <deepali.moraskar95@gmail.com>
2025-08-09 00:22:05 +0200

    lint error fixes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -12,7 +12,7 @@ const Products = () => {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(data);
   const [loading, setLoading] = useState(false);
-  let componentMounted = true;
+  const componentMounted = React.useRef(true);
 
   const dispatch = useDispatch();
 
@@ -24,14 +24,14 @@ const Products = () => {
     const getProducts = async () => {
       setLoading(true);
       const response = await fetch("https://fakestoreapi.com/products/");
-      if (componentMounted) {
+      if (componentMounted.current) {
         setData(await response.clone().json());
         setFilter(await response.json());
         setLoading(false);
       }
 
       return () => {
-        componentMounted = false;
+        componentMounted.current = false;
       };
     };
 
